5508df37b023a5575bce991ed50336bf69b34ec1
Hellbis <welder.marques10@gmail.com>
2019-09-26 08:18:38 -0300

    VerificadorLexico

--- src/Analisador/Lexico/AnalisadorLexico.java
+++ src/Analisador/Lexico/AnalisadorLexico.java
@@ -4,17 +4,33 @@ import Common.IntegerStringPair;
 import Common.Config;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import Lista.Lista;
 
 public class AnalisadorLexico {
-    private char[] letter = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    private char[] digt = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+//    private char[] letter = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     private ArrayList<String> operator;
+    private ArrayList<String> digt;
+    private ArrayList<String> letter;
     public AnalisadorLexico()
     {
         operator = new ArrayList<>();
+        digt = new ArrayList<>();
+//
+        letter = new ArrayList<>(Arrays.asList (new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}));
+        digt.add("0");
+        digt.add("1");
+        digt.add("2");
+        digt.add("3");
+        digt.add("4");
+        digt.add("5");
+        digt.add("6");
+        digt.add("7");
+        digt.add("8");
+        digt.add("9");
         operator.add("+");
         operator.add("-");
         operator.add("/");
@@ -24,6 +40,7 @@ public class AnalisadorLexico {
         operator.add(">");
         operator.add("<=");
         operator.add(">=");
+
     }
 
     public void analisador()
@@ -34,8 +51,19 @@ public class AnalisadorLexico {
 
          for (String linha : linhas)
          {
-             for (int i = 0; i < linha.length(); i++) {
-                 operator.contains(linha[i]);
+             for (int i = 0; i < linha.length(); i++)
+             {
+                 if (!letter.contains(Character.toString(linha.charAt(i))))
+                 {
+                     System.out.println(linha.charAt(i));
+                     System.out.println("Meu pinto");
+                 }
+                 else
+                 {
+                     System.out.println(linha.charAt(i));
+                     System.out.println("Meu pinto kkk");
+                 }
+
              }
          }
 //        try (BufferedWriter arquivoSaida = new BufferedWriter(new FileWriter(Config.NOME_ARQUIVO_ESCRITA))) {
